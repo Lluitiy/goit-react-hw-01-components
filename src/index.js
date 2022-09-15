2a3bbd9f626e0f7db1ce062d8e43957f42669e48
Pavlo Vovzhynskyy <pavlovovzhynskyy@gmail.com>
2022-09-15 19:48:53 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App/App';
+import { ThemeProvider } from '@emotion/react';
 import './index.css';
+
+const theme = {
+	colors: {
+		// добовляем цвета которые нам необходимы и они будут
+		//досупны как пропсы. Например theme.colors.имя колора
+		//главное передать их в нашем компоненте
+
+	}
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<App />		
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>
 );
